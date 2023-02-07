7d4881929e8be67b5432dae8e32fb1921a5d00a4
mccrush <mccrush2027@gmail.com>
2023-02-07 14:06:49 +0700

    Settings card view

--- src/modules/info/InfoCard.vue
+++ src/modules/info/InfoCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card bg-light bg-gradient shadow-sm border-0 mt-3">
+  <div class="card bg-light bg-gradient shadow border-0 mt-3">
     <div class="card-body">
       <h5 class="card-title">{{ item.title }}</h5>
       <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
@@ -7,8 +7,8 @@
       <a href="#" class="card-link">Card link</a>
       <a href="#" class="card-link">Another link</a>
     </div>
-    <div class="card-footer text-muted">
-      Дата обновления {{ item.dateUpdate }}
+    <div class="card-footer">
+      <code class="text-muted">Дата обновления: {{ item.dateUpdate }}</code>
     </div>
   </div>
 </template>
--- src/modules/info/interface/BtnMenu.vue
+++ src/modules/info/interface/BtnMenu.vue
@@ -1,18 +1,13 @@
 <template>
   <button class="btn w-100" :title="title">
     <div>{{ title }}</div>
-    <!-- <div class="mt-1">{{ subTitle }}</div> -->
   </button>
 </template>
 
 <script>
 export default {
   props: {
-    title: String,
-    subTitle: {
-      type: String,
-      default: '---'
-    }
+    title: String
   }
 }
 </script>
