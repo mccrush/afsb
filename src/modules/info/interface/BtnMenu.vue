b26e35c89da29ede7b1035b62788783b953630dc
mccrush <mccrush2027@gmail.com>
2023-02-14 11:21:29 +0700

    Update button height

--- src/modules/info/interface/BtnMenu.vue
+++ src/modules/info/interface/BtnMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="btn w-100 lh-1 pt-2 pb-2" :title="title">
+  <button class="h-53 btn w-100 lh-sm pt-2 pb-2" :title="title">
     <div class="small">{{ title }}</div>
   </button>
 </template>
@@ -11,3 +11,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.h-53 {
+  height: 53px;
+}
+</style>
